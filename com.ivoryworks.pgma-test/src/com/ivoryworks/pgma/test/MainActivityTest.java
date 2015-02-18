f20cc00211a7c19a9b27f98b23d3bcf7bda54f1c
ivoryworks <ivoryworkz@gmail.com>
2015-02-18 17:57:30 +0900

    create MainActivityTest.java

---
+++ com.ivoryworks.pgma-test/src/com/ivoryworks/pgma/test/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.ivoryworks.pgma.test;
+
+import com.ivoryworks.pgma.MainActivity;
+
+import android.test.AndroidTestCase;
+
+public class MainActivityTest extends AndroidTestCase {
+
+    public MainActivityTest() {
+
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        // TODO 自動生成されたメソッド・スタブ
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        // TODO 自動生成されたメソッド・スタブ
+        super.tearDown();
+    }
+
+}
