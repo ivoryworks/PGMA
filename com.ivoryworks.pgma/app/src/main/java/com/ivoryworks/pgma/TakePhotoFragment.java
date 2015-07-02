a997c189c926ee7548756033a3e36875e496a8bc
ivoryworks <ivoryworkz@gmail.com>
2015-07-02 14:42:22 +0900

    [add]カメラ連携機能の追加
    Intentによるカメラ起動とImageViewへの取り込みを実装
    * ImageView上での画像向きが不正である問題あり

--- com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/TakePhotoFragment.java
+++ com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/TakePhotoFragment.java
@@ -1,14 +1,27 @@
 package com.ivoryworks.pgma;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.ImageView;
 
-public class TakePhotoFragment extends Fragment {
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TakePhotoFragment extends Fragment implements View.OnClickListener {
+
+    private final int REQ_CODE_ACTION_IMAGE_CAPTURE = 1;
+    private File mDCIM;
+    private Uri mPhotoUri;
+    private ImageView mPreviewPhoto;
 
     /**
      * Use this factory method to create a new instance of
@@ -29,13 +42,24 @@ public class TakePhotoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // DCIM directory
+        mDCIM = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        String timeStamp = new SimpleDateFormat(
+                "yyyy_MM_dd__HH_mm_ss").format(new Date());
+        File mediaFile = new File(mDCIM.getPath() + File.separator + timeStamp + ".jpg");
+        mPhotoUri = Uri.fromFile(mediaFile);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_take_photo, container, false);
+        View layoutView = inflater.inflate(R.layout.fragment_take_photo, container, false);
+
+        layoutView.findViewById(R.id.btnCamera).setOnClickListener(this);
+        mPreviewPhoto = (ImageView) layoutView.findViewById(R.id.previewPhoto);
+
+        return layoutView;
     }
 
     @Override
@@ -48,4 +72,29 @@ public class TakePhotoFragment extends Fragment {
         super.onDetach();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.btnCamera:
+            //mPhotoUri = Uri.parse(mDCIM.getPath() + "/test.jpg");
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoUri);
+            startActivityForResult(intent, REQ_CODE_ACTION_IMAGE_CAPTURE);
+            break;
+        default:
+            break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case REQ_CODE_ACTION_IMAGE_CAPTURE:
+            if (resultCode == Activity.RESULT_OK) {
+                mPreviewPhoto.setImageURI(mPhotoUri);
+            }
+            break;
+        }
+    }
 }
