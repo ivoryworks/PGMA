a866e2c65c272dab38f515207809d0f5c85307be
ivoryworks <ivoryworkz@gmail.com>
2015-07-29 20:08:03 +0900

    [modify]MediaEffects:octocatの表示

--- com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/MediaEffectsFragment.java
+++ com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/MediaEffectsFragment.java
@@ -1,5 +1,8 @@
 package com.ivoryworks.pgma;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.effect.EffectContext;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,11 +12,18 @@ import android.view.ViewGroup;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
 
 public class MediaEffectsFragment extends Fragment implements GLSurfaceView.Renderer {
 
     public static String TAG = MediaEffectsFragment.class.getSimpleName();
     private GLSurfaceView mEffectView;
+    private int[] mTextures = new int[2];
+    private EffectContext  mEffectContext;
+    private TextureRenderer mTexRenderer = new TextureRenderer();
+    private int mImageWidth;
+    private int mImageHeight;
 
     public static MediaEffectsFragment newInstance() {
         return new MediaEffectsFragment();
@@ -41,11 +51,38 @@ public class MediaEffectsFragment extends Fragment implements GLSurfaceView.Rend
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+        if (mTexRenderer != null) {
+            mTexRenderer.updateViewSize(width, height);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        if (mEffectContext == null) {
+            mEffectContext = EffectContext.createWithCurrentGlContext();
+            mTexRenderer.init();
+            loadTextures();
+        }
+        renderResult();
+    }
 
+    private void loadTextures() {
+        // Generate textures
+        GLES20.glGenTextures(2, mTextures, 0);
+        // Load input bitmap
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.octocat_l);
+        mImageWidth = bitmap.getWidth();
+        mImageHeight = bitmap.getHeight();
+        mTexRenderer.updateTextureSize(mImageWidth, mImageHeight);
+        // Upload to texture
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextures[0]);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+        // Set texture parameters
+        GLToolbox.initTexParams();
+    }
+
+    private void renderResult() {
+        mTexRenderer.renderTexture(mTextures[0]);
     }
 }
