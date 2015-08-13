5921037d5b2d01f1171ebae2218107d039687f27
ivoryworks <ivoryworkz@gmail.com>
2015-08-13 09:34:05 +0900

    [modify]オリジナル画像(beetle)の読み込み表示

--- com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/ScaleTypeFragment.java
+++ com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/ScaleTypeFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,6 +31,31 @@ public class ScaleTypeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scale_type, container, false);
+        ImageView originImageView = (ImageView) view.findViewById(R.id.originImageView);
+        Bitmap beetleBitmap = loadAssetImage(Constants.BEETLE_FILENAME, originImageView.getWidth());
+        originImageView.setImageBitmap(beetleBitmap);
+
         return view;
+    }
+
+    private Bitmap loadAssetImage(String name, int reqWidth) {
+        AssetManager manager = getResources().getAssets();
+        InputStream inputStreamBase = null;
+        try {
+            inputStreamBase = manager.open(name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (inputStreamBase != null) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(inputStreamBase, null, options);
+
+            options.inSampleSize = reqWidth / options.outWidth;
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeStream(inputStreamBase, null, options);
+        }
+        return null;
     }
 }
