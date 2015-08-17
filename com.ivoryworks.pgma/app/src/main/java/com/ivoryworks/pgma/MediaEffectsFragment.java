5e8c38d057c76be155307fdcf2021b77ce955866
ivoryworks <ivoryworkz@gmail.com>
2015-08-17 13:46:35 +0900

    [modify]MediaEffects:90度回転

--- com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/MediaEffectsFragment.java
+++ com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/MediaEffectsFragment.java
@@ -168,12 +168,22 @@ public class MediaEffectsFragment extends Fragment implements GLSurfaceView.Rend
             case R.id.menu_item_negative:
                 mEffect = effectFactory.createEffect(EffectFactory.EFFECT_NEGATIVE);
                 break;
+            case R.id.menu_item_rotate90:
+                mEffect = effectFactory.createEffect(EffectFactory.EFFECT_ROTATE);
+                mEffect.setParameter("angle", 90);
+                break;
             default:
                 break;
         }
     }
 
     private void applyEffect() {
-        mEffect.apply(mTextures[0], mImageWidth, mImageHeight, mTextures[1]);
+        if (mCurrentEffect == R.id.menu_item_rotate90) {
+            mTexRenderer.updateTextureSize(mImageHeight, mImageWidth);
+            mEffect.apply(mTextures[0], mImageHeight, mImageWidth, mTextures[1]);
+        } else {
+            mTexRenderer.updateTextureSize(mImageWidth, mImageHeight);
+            mEffect.apply(mTextures[0], mImageWidth, mImageHeight, mTextures[1]);
+        }
     }
 }
