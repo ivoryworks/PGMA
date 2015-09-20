145b022e78facfd1d5c965b5d47e862812990e5a
ivoryworks <ivoryworkz@gmail.com>
2015-09-20 22:57:03 +0900

    [modify]PickImage:testShareFail
    画像未選択時のメッセージ表示を確認

--- com.ivoryworks.pgma/app/src/androidTest/java/com/ivoryworks/pgma/PickImageFragmentTest.java
+++ com.ivoryworks.pgma/app/src/androidTest/java/com/ivoryworks/pgma/PickImageFragmentTest.java
@@ -1,15 +1,20 @@
 package com.ivoryworks.pgma;
 
+import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
 import android.support.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
@@ -29,6 +34,16 @@ public class PickImageFragmentTest {
     @Test
     public void testShareFail() {
         // open Navigation drawer
-        mDevice.findObject(By.desc("Navigate up")).click();
+        Tools.openNavigationDrawer(mDevice);
+
+        try {
+            mDevice.findObject(new UiSelector().text("Pick image")).clickAndWaitForNewWindow();
+            mDevice.findObject(new UiSelector().description("share")).click();
+        } catch (UiObjectNotFoundException e) {
+            e.printStackTrace();
+        }
+        assertTrue(mDevice.findObject(new UiSelector().text("Share file can not be found.")).exists());
+
+        SystemClock.sleep(5000);
     }
 }
