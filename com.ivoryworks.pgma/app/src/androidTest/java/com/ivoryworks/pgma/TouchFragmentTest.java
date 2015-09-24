ee95f07199c35cab398560a938c204090ca40852
ivoryworks <ivoryworkz@gmail.com>
2015-09-24 14:52:27 +0900

    [add]Touch:create TouchFragment UiAutomator test base.

---
+++ com.ivoryworks.pgma/app/src/androidTest/java/com/ivoryworks/pgma/TouchFragmentTest.java
@@ -0,0 +1,38 @@
+package com.ivoryworks.pgma;
+
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class TouchFragmentTest {
+    private UiDevice mDevice;
+
+    @Before
+    public void setup() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        Tools.goToHome(mDevice);
+        Tools.startPGMA(mDevice);
+
+        // open Navigation drawer
+        Tools.openNavigationDrawer(mDevice);
+        try {
+            mDevice.findObject(new UiSelector().text("Touch")).clickAndWaitForNewWindow();
+        } catch (UiObjectNotFoundException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+}
