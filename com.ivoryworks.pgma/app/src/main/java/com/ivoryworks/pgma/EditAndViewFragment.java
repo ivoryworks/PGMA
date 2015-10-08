92b929c2eb19b88266d6607d648c2b33a6cf192c
ivoryworks <ivoryworkz@gmail.com>
2015-10-08 12:14:59 +0900

    [modify]EditAndText:EditTextへの入力をTextViewにリアルタイム反映させる
    set TextWatcher

--- com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/EditAndViewFragment.java
+++ com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/EditAndViewFragment.java
@@ -2,15 +2,19 @@ package com.ivoryworks.pgma;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class EditAndViewFragment extends Fragment {
+public class EditAndViewFragment extends Fragment implements TextWatcher {
 
     public static String TAG = EditAndViewFragment.class.getSimpleName();
-    private TextView mLogText;
+    EditText mEditText;
+    TextView mTextView;
 
     public static EditAndViewFragment newInstance() {
         return new EditAndViewFragment();
@@ -24,6 +28,26 @@ public class EditAndViewFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_and_view, container, false);
 
+        mTextView = (TextView) view.findViewById(R.id.textView);
+
+        mEditText = (EditText) view.findViewById(R.id.editText);
+        mEditText.addTextChangedListener(this);
+
         return view;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        mTextView.setText(mEditText.getText());
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
     }
 }
