078b541fb2f28253c2dd230d09fb1d04a509a985
ivoryworks <ivoryworkz@gmail.com>
2015-10-20 21:32:52 +0900

    [add]ImageRotateFragmentTest:create ImageRotate test

---
+++ com.ivoryworks.pgma/app/src/androidTest/java/com/ivoryworks/pgma/ImageRotateFragmentTest.java
@@ -0,0 +1,43 @@
+package com.ivoryworks.pgma;
+
+import android.os.RemoteException;
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class ImageRotateFragmentTest {
+    private UiDevice mDevice;
+
+    @Before
+    public void setup() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        Tools.goToHome(mDevice);
+        Tools.startPGMA(mDevice);
+
+        // open Navigation drawer
+        Tools.clickNavigationDrawerItem(mDevice, "Image rotate");
+    }
+
+    @Test
+    public void testOrientation() {
+        try {
+            mDevice.setOrientationLeft();
+            mDevice.setOrientationRight();
+            mDevice.setOrientationNatural();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        SystemClock.sleep(5000);
+    }
+}
