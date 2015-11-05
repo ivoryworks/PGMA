20d37d094d1dec59bfc8ce0bcbf8f37934a2e69d
ivoryworks <ivoryworkz@gmail.com>
2015-11-05 11:42:38 +0900

    [modify]Notification:add Simple notification(icon only)
    最も単純なNotification。アイコンのみ表示する。

--- com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/NotificationFragment.java
+++ com.ivoryworks.pgma/app/src/main/java/com/ivoryworks/pgma/NotificationFragment.java
@@ -1,14 +1,22 @@
 package com.ivoryworks.pgma;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class NotificationFragment extends Fragment {
+import static android.support.v7.app.NotificationCompat.*;
+
+public class NotificationFragment extends Fragment implements View.OnClickListener{
 
     public static String TAG = NotificationFragment.class.getSimpleName();
+    private final int NOTIFICATION_ICON_ONLY = 1;
+    private Context mContext;
 
     public static NotificationFragment newInstance() {
         return new NotificationFragment();
@@ -20,8 +28,22 @@ public class NotificationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mContext = getActivity().getApplicationContext();
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
-
+        Button btnSimple = (Button) view.findViewById(R.id.button_simple);
+        btnSimple.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.button_simple:
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            NotificationManagerCompat manager = NotificationManagerCompat.from(mContext);
+            manager.notify(NOTIFICATION_ICON_ONLY, builder.build());
+            break;
+        }
     }
 }
