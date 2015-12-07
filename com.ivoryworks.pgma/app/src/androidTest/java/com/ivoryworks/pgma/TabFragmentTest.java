a49a8a60eec5ee7e5acf096cca7251fbceb7eded
ivoryworks <ivoryworkz@gmail.com>
2015-12-07 20:42:54 +0900

    [add]TabFragmentTest:add testOrientation()

---
+++ com.ivoryworks.pgma/app/src/androidTest/java/com/ivoryworks/pgma/TabFragmentTest.java
@@ -0,0 +1,40 @@
+package com.ivoryworks.pgma;
+
+import android.os.RemoteException;
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class TabFragmentTest {
+    private UiDevice mDevice;
+
+    @Before
+    public void setup() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        Tools.goToHome(mDevice);
+        Tools.startPGMA(mDevice);
+
+        // open Navigation drawer
+        Tools.clickNavigationDrawerItem(mDevice, "Tab");
+    }
+
+    @Test
+    public void testOrientation() {
+        try {
+            Tools.orientation(mDevice);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+    }
+}
